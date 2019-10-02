4c6625ec6186b397630ab5cfe7dfe5bda6e7084f
Saggre <sakri.koskimies@hotmail.com>
2019-10-02 05:45:46 +0200

    Develop rendering and core

---
+++ Player.cs
@@ -0,0 +1,12 @@
+﻿// Created by Sakri Koskimies (Github: Saggre) on 02/10/2019
+
+using EconSim.Core;
+using Microsoft.Xna.Framework;
+
+namespace EconSim
+{
+    public class Player : GameObject
+    {
+
+    }
+}
