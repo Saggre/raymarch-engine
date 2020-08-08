322a921448632fb6b867110077355ffbd4f9dff5
Saggre <sakri.koskimies@hotmail.com>
2020-08-08 21:25:01 +0300

    Develop raymarching camera move

--- Shaders/Raymarch/Utils.hlsl
+++ Shaders/Raymarch/Utils.hlsl
@@ -1,4 +1,4 @@
-float smin(float a, float b, float k)
+float smoothMin(float a, float b, float k)
 {
 	float h = clamp(0.5 + 0.5 * (a - b) / k, 0.0, 1.0);
 	return lerp(a, b, h) - k * h * (1.0 - h);
